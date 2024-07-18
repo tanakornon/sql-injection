4f3a7df4b1952aedf01b7b45c4fa8adb8d06367c
Tanakorn Onlamoon <tanakorn.phinx@gmail.com>
2024-07-18 15:02:14 +0700

    fix: add init db

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import sqlite3
 
+db_path = "database.db"
+
 def main():
     user_input = "1 OR 1=1"
 
-    vulnerable_get_user(user_input)
-    secure_get_user(user_input)
+    init_db()
+    print(vulnerable_get_user(user_input))
+    print(secure_get_user(user_input))
+
+
+def init_db():
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL
+            )
+            """
+        )
+
+        cursor.execute(
+            """
+            INSERT OR IGNORE INTO users (username, password) VALUES
+            ('admin', 'admin'),
+            ('user', 'user')
+            """
+        )
+
+        conn.commit()
 
 
 def vulnerable_get_user(user_id: str):
-    with sqlite3.connect('example.db') as conn:
+    with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
 
         query = f"SELECT * FROM users WHERE id = {user_id}"
@@ -21,7 +49,7 @@ def vulnerable_get_user(user_id: str):
 
 
 def secure_get_user(user_id: str):
-    with sqlite3.connect('example.db') as conn:
+    with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
 
         query = f"SELECT * FROM users WHERE id = ?"
